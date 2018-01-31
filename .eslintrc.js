d0f7269dcea8903ba110607a03661e60a617c94a
tsanie <tsorgy@gmail.com>
2018-01-31 11:03:55 +0800

    initial

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  extends: 'google',
+  plugins: [
+    'html'
+  ],
+  rules: {
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  },
+  env: {
+    browser: true
+  }
+};
